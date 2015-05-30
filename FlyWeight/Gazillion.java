86ec7abce0cff6adb3e270d5991508dad62a79c9
DAT14V1 - Casper Vejdiksen <casp539b@stud.kea.dk>
2015-05-30 16:15:07 +0200

    Create Gazillion.java

---
+++ FlyWeight/Gazillion.java
@@ -0,0 +1,17 @@
+public class Gazillion
+{
+	private static int num = 0;
+	private int row, col;
+	
+	public Gazillion(int maxPerRow)
+	{
+		row = num/maxPerRow;
+		col = num%maxPerRow;
+		num++;
+	}
+	
+	void report()
+	{
+		System.out.print(" " + row + col);
+	}
+}
