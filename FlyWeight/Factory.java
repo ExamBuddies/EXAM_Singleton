6206478a62f4550e509491ab21d8d84c90edf9a7
DAT14V1 - Casper Vejdiksen <casp539b@stud.kea.dk>
2015-05-30 16:19:52 +0200

    Create Factory.java

---
+++ FlyWeight/Factory.java
@@ -0,0 +1,18 @@
+public class Factory
+{
+	private Gazillion[] pool;
+	
+	public Factory(int maxRows)
+	{
+		pool = new Gazillion[maxRows];
+	}
+	
+	public Gazillion getFlyweight(int theRow)
+	{
+		if(pool[theRow] == null)
+		{
+			pool[theRow] = new Gazillion(theRow);
+		}
+			return pool[theRow];
+	}
+}
